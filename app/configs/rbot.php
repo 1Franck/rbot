a62503a33971087cfb47b482293cd210aa898d25
Francois Lajoie <snake386@hotmail.com>
2015-05-24 11:13:53 -0400

    added auth conf example

--- app/configs/rbot.php
+++ app/configs/rbot.php
@@ -2,9 +2,15 @@
 
 return [
     'dev' => [
+        'auth' => [
+            'hash'          => 'sha512',
+            // admin / admin
+            'user_hash'     => 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec',
+            'password_hash' => 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec',
+        ],
         'db' => [
             'driver'    => 'mysql',
-            'host'      => 'localhost',
+            'host'      => '127.0.0.1',
             'database'  => 'rbot_dev',
             'username'  => 'root',
             'password'  => '',

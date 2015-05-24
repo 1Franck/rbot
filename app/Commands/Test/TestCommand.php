6da2ffcdec6fa70d74c1a951a4815b54405c1f4f
Francois Lajoie <snake386@hotmail.com>
2015-05-23 20:31:00 -0400

    renamed var _specs

--- app/Commands/Test/TestCommand.php
+++ app/Commands/Test/TestCommand.php
@@ -24,38 +24,38 @@ class TestCommand extends Command
      */
     public function setOptions() 
     {
-        $this->_specs->add('h|help', 'test' );
-        $this->_specs->add('f|foo:', 'option requires a value.' )
+        $this->_options->add('h|help', 'test' );
+        $this->_options->add('f|foo:', 'option requires a value.' )
             ->isa('String');
 
-        $this->_specs->add('b|bar+', 'option with multiple value.' )
+        $this->_options->add('b|bar+', 'option with multiple value.' )
             ->isa('Number');
 
-        $this->_specs->add('z|zoo?', 'option with optional value.' )
+        $this->_options->add('z|zoo?', 'option with optional value.' )
             ->isa('Boolean')
             ;
 
-        $this->_specs->add('o|output?', 'option with optional value.' )
+        $this->_options->add('o|output?', 'option with optional value.' )
             ->isa('File');
             //->defaultValue('output.txt');
 
         // works for -vvv  => verbose = 3
-        $this->_specs->add('v|verbose', 'verbose')
+        $this->_options->add('v|verbose', 'verbose')
             ->isa('Number')
             ->incremental();
 
-        $this->_specs->add('file:', 'option value should be a file.' )
+        $this->_options->add('file:', 'option value should be a file.' )
             ->trigger(function($value) {
                 echo "Set value to :";
                 var_dump($value);
             })
             ->isa('File');
 
-        $this->_specs->add('d|debug', 'debug message.' );
-        $this->_specs->add('long', 'long option name only.' );
-        $this->_specs->add('s', 'short option name only.' );
-        $this->_specs->add('m', 'short option m');
-        $this->_specs->add('4', 'short option with digit');
+        $this->_options->add('d|debug', 'debug message.' );
+        $this->_options->add('long', 'long option name only.' );
+        $this->_options->add('s', 'short option name only.' );
+        $this->_options->add('m', 'short option m');
+        $this->_options->add('4', 'short option with digit');
     }
 
     /**

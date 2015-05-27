9b4827fc39cb46d1e4dc65aac88dd9d72cc1d0d2
Francois Lajoie <snake386@hotmail.com>
2015-05-27 17:35:30 -0400

    updated header

--- rbot/Exception/ConfigMissing.php
+++ rbot/Exception/ConfigMissing.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the RBot.
+ *
+ * (c) Francois Lajoie <o_o@francoislajoie.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace RBot\Exception;
 
 class ConfigMissing extends GenericException {}

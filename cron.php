392053f873760a6101e86980e61e1abd54a4ce8f
Francois Lajoie <snake386@hotmail.com>
2015-06-04 13:08:46 -0400

    updated header

--- cron.php
+++ cron.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the RBot.
+ *
+ * (c) Francois Lajoie <o_o@francoislajoie.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace App\Commands;
 if(!defined('NS_APP')) define('NS_APP', __NAMESPACE__.'\\');
 
